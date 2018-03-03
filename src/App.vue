d86208a0769086fd8911227219d12455f39d6c2a
homahi <homahi0128@gmail.com>
2018-03-03 22:44:56 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<gmap-map
+  :center="{lat:35.664334, lng:139.750129}"
+  :zoom="18"
+  map-type-id="terrain"
+  style="width: 500px; height: 300px"
+></gmap-map>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
